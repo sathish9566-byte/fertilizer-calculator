bb820d093e032f9ed72b084070997d256a7fc657
sathish9566-byte <sathish9566.d@gmail.com>
2025-10-21 09:35:52 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'fertilizer-calculator-v1';
+const urlsToCache = [
+  '/',
+  'index.html'
+];
+
+// Install the service worker and cache the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch files from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
